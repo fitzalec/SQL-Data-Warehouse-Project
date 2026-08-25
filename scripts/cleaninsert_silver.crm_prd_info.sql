90d67f7ee6079722cf8305cfb2690c73a4c621b6
fitzalec <alecfitzgerald37@gmail.com>
2026-08-25 15:35:53 -0400

    Add files via upload

---
+++ scripts/cleaninsert_silver.crm_prd_info.sql
@@ -0,0 +1,29 @@
+INSERT INTO silver.crm_prd_info(
+	prd_id,
+	cat_id,
+	prd_key,
+	prd_nm,
+	prd_cost,
+	prd_line,
+	prd_start_dt,
+	prd_end_dt
+)
+SELECT
+	prd_id,
+	--Extracting category ID and product key for analytics--
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+	prd_nm,
+	ISNULL(prd_cost, 0) AS prd_cost,
+	--Map product line codes to descriptive values--
+	CASE UPPER(TRIM(prd_line))
+		 WHEN 'M' THEN 'Mountain'
+		 WHEN 'R' THEN 'Road'
+		 WHEN 'S' THEN 'Other Sales'
+		 WHEN 'T' THEN 'Touring'
+		 ELSE 'N/A'
+	END AS prd_line,
+	CAST(prd_start_dt AS DATE) AS prd_start_dt,
+	--Calculating end date as one day before the next start date for consistency and clarity for cost--
+	CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) as prd_end_dt
+FROM bronze.crm_prd_info
