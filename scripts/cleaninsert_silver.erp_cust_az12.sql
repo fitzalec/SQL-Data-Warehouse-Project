22462478ccc17436b2cffbd262ad6a702e5e1b8b
fitzalec <alecfitzgerald37@gmail.com>
2026-08-25 20:49:22 -0400

    Refactor SQL insert statement for clarity

--- scripts/cleaninsert_silver.erp_cust_az12.sql
+++ scripts/cleaninsert_silver.erp_cust_az12.sql
@@ -2,15 +2,18 @@ INSERT INTO silver.erp_cust_az12 (cid, bdate, gen)
 
 SELECT
 	--Remove 'NAS' prefix from historical records when present for consistency--
-	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+	CASE 
+		 WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
 		 ELSE cid
 	END cid,
 	--Set any birthdates not applicable (in future) to NULL--
-	CASE WHEN bdate > GETDATE() THEN NULL
+	CASE 
+		 WHEN bdate > GETDATE() THEN NULL
 		 ELSE bdate
 	END bdate,
 	--Normalize gender values to handle unknown cases--
-	CASE WHEN UPPER(TRIM(gen)) IN ('F', 'Female') THEN 'Female'
+	CASE 
+		 WHEN UPPER(TRIM(gen)) IN ('F', 'Female') THEN 'Female'
 		 WHEN UPPER(TRIM(gen)) IN ('M', 'Male') THEN 'Male'
 		 ELSE 'N/A'
 	END gen
