d4e421f60fdd5ae0729ae988d9f696d7438f5110
fitzalec <alecfitzgerald37@gmail.com>
2026-08-25 20:43:59 -0400

    Refactor SQL insert statement for crm_prd_info

--- scripts/cleaninsert_silver.crm_prd_info.sql
+++ scripts/cleaninsert_silver.crm_prd_info.sql
@@ -25,5 +25,5 @@ SELECT
 	END AS prd_line,
 	CAST(prd_start_dt AS DATE) AS prd_start_dt,
 	--Calculating end date as one day before the next start date for consistency and clarity for cost--
-	CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) as prd_end_dt
+	CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
 FROM bronze.crm_prd_info
